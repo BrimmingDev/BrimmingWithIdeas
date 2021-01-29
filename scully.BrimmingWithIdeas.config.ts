cd1f474469665a0f75812e41a7e9aa61be78cdc9
Evan Brim <evanbrim84@gmail.com>
2021-01-29 18:55:56 -0500

    Add Scully

---
+++ scully.BrimmingWithIdeas.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./src",
+  projectName: "BrimmingWithIdeas",
+  outDir: './dist/static',
+  routes: {
+  }
+};
